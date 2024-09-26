862b8d59e8a919e317e2edbd44bac3a24240d01b
Tyler Gallup <galluptj@gmail.com>
2024-09-25 17:29:24 -0700

    Adding recommended article pipeline and many other improvements

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 import streamlit as st
-from faq_pipeline import *
+import faq_pipeline as fp
 import asyncio
 import warnings
+import importlib
+
 warnings.filterwarnings("ignore")
+importlib.reload(fp)
+
+def set_url(url):
+    st.session_state.url_input = url
 
 st.set_page_config(page_title="FAQ Generator",
                     page_icon="faq-logo.png"
 )
-
+st.cache_data.clear()
 st.title("FAQ Generator")
 
 st.markdown("""
     <style>
     input[type="text"] {
-        font-size: 16px !important;
+        font-size: 14px !important;
     }
-    p {
+    label[for="url_input"] {
         font-size: 20px !important;
     }
     div {
@@ -24,29 +30,59 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-url = st.text_input("Enter website URL to generate an FAQ for:")
+url = st.text_input("Enter website URL to generate an FAQ for:", key='url_input')
 async def generate_faq(url):
     status_text = st.empty()
     progress_bar = st.progress(0)
 
     status_text.text("Loading website content...")
-    html = await load_html(url)
-    doc_string = extract_from_html(html)
+    html = await fp.load_html(url)
+    doc_string, article_title = fp.extract_from_html(html)
     progress_bar.progress(33)
 
     status_text.text("Summarizing content...")
-    final_summary = summarize_content(doc_string)
+    final_summary = fp.summarize_content(doc_string)
     progress_bar.progress(66)
 
     status_text.text("Generating FAQ...")
-    questions, answers = prompt_llm(final_summary)
+    topic_list = fp.prompt_llm_for_related_topics(final_summary)
+    rec_titles, rec_links = [], []
+    results = fp.search_google(topic_list[0])
+    new_rec_titles, new_rec_links = fp.get_top_5_articles(results, url)
+    rec_titles += new_rec_titles
+    rec_links += new_rec_links
+    questions, answers = fp.prompt_llm(final_summary)
+    
+    results = fp.search_google(topic_list[1])
+    new_rec_titles, new_rec_links = fp.get_top_5_articles(results, url)
+    rec_titles += new_rec_titles
+    rec_links += new_rec_links
+
     progress_bar.progress(100)
 
     status_text.text("FAQ generation complete...")
+    st.markdown(f"## {article_title}")
     for i in range(len(questions)):
         expand_faq = st.expander(questions[i])
         with expand_faq:
             st.write(answers[i])
+    st.write(f"Article Summary: {final_summary}")
+    
+    # for topic in topic_list:
+    #     results = fp.search_google(topic)
+    #     new_rec_titles, new_rec_links = fp.get_top_5_articles(results, url)
+    #     rec_titles += new_rec_titles
+    #     rec_links += new_rec_links
+        # print(topic)
+    # print(topic_list)
+    
+    st.sidebar.header("Recommended Articles")
+    for i, title in enumerate(rec_titles):
+        if st.sidebar.button(title, key=i, on_click=set_url, args=(rec_links[i],)):
+            # set_url(rec_links[i])
+            generate_faq(rec_links[i])
+    # st.sidebar.selectbox("Choose an article: ", rec_titles)
+
 if url:
     # with st.spinner("Generating FAQ..."):
     asyncio.run(generate_faq(url))
--- faq_pipeline.py
+++ faq_pipeline.py
@@ -3,30 +3,41 @@ from langchain_community.document_transformers import BeautifulSoupTransformer
 from transformers import pipeline
 import openai
 import re
+import os
+import requests
+import tensorflow as tf
+import ast
 
 async def load_html(url):
-    print(url)
     loader = AsyncHtmlLoader(url)
     html = loader.load()
     return html
 
 def extract_from_html(html):
     bs_transformer = BeautifulSoupTransformer()
-    tags_to_extract = ["p", "span", "div", "h1", "h2", "h3", "h4", "h5", "h6"]
+    tags_to_extract = ["p", "span", "div", "h1", "h2", "h3", "h4", "h5", "h6", "li"]
     docs_transformed = bs_transformer.transform_documents(html, tags_to_extract=tags_to_extract)
     doc_string = ""
     for doc in docs_transformed:
         doc_string += doc.page_content
-    return doc_string
+    titles_transformed = bs_transformer.transform_documents(html, tags_to_extract=["title"])
+    article_title = titles_transformed[0].metadata['title']
+    return doc_string, article_title
 
 def summarize_content(doc_string):
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn", device="cpu")
+    physical_devices = tf.config.list_physical_devices('GPU')
+    if physical_devices:
+        print("GPU detected, using GPU for summarization")
+        device = 0
+    else:
+        print("No GPU detected, using CPU")
+        device = -1
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn", device=device)
     max_chunk_size = 1000
-    inputs = summarizer.tokenizer(doc_string, return_tensors="pt", truncation=False)
+    inputs = summarizer.tokenizer(doc_string, return_tensors="tf", truncation=False)
     tokens = inputs.input_ids[0]
     chunks = [tokens[i:i+max_chunk_size] for i in range(0, len(tokens), max_chunk_size)]
     batch_chunk_texts = [summarizer.tokenizer.decode(chunk, skip_special_tokens=True) for chunk in chunks]
-    # summaries = []
     summaries = summarizer(batch_chunk_texts, max_length=1000, truncation=True) 
     summary_texts = [summary['summary_text'] for summary in summaries]
     final_summary = " ".join(summary_texts)
@@ -37,6 +48,7 @@ def prompt_llm(final_summary):
     Based on the following content: {final_summary} generate 10-15 questions 
     that will help readers understand the content better then provide
     informative answers to these questions.
+    Only give questions that can be answered from the content of the article.
     Provide these answers in the format Question: Answer
     Make sure you don't use LaTeX in your questions and answers.
     """
@@ -55,3 +67,47 @@ def prompt_llm(final_summary):
     questions = [faq for i, faq in enumerate(faq_list) if i % 2 == 0]
     answers = [faq for i, faq in enumerate(faq_list) if i % 2 == 1]
     return questions, answers
+
+def prompt_llm_for_related_topics(final_summary):
+    prompt = f"""
+    Based on the following content: {final_summary} generate exactly 2 topics
+    that are related to this topic or discussed in the article summary. 
+    Provide your answer in the format '['Topic 1', 'Topic 2']'
+    """
+
+
+    response = openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant who generates topics related to article summaries."},
+        {"role": "user", "content": prompt},
+    ]
+    )
+    topic_list_str = response.choices[0].message.content
+    topic_list = ast.literal_eval(topic_list_str)
+    return topic_list
+
+
+def search_google(query):
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+    SEARCH_ENGINE_ID = "f35b11676565b4fb5"
+    url = f"https://www.googleapis.com/customsearch/v1"
+    params = {
+        "key": GOOGLE_API_KEY,
+        "cx": SEARCH_ENGINE_ID,
+        "q": query,
+    }
+    response = requests.get(url, params=params)
+    results = response.json()
+    return results
+
+def get_top_5_articles(results, past_url):
+    titles = []
+    links = []
+    for i in range(5):
+        if 'items' in results and results['items'][i]['link'] != past_url:
+            titles.append(results['items'][i]['title'])
+            links.append(results['items'][i]['link'])
+        else:
+            print(results)
+    return titles, links
