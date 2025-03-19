888a89cdfeef05675402ab2add5c9b9d56ca6e12
Tyler Gallup <galluptj@gmail.com>
2025-03-18 21:18:01 -0700

    Testing Amplify deployment

--- backend/lambda.py
+++ backend/lambda.py
@@ -1,11 +1,9 @@
 from mangum import Mangum
 import os
+from main import app
 
 # Set environment variables before importing the app
 os.environ['ENVIRONMENT'] = 'production'
-
-# Import the FastAPI app
-from main import app
 
 # Create the handler
 handler = Mangum(app)
